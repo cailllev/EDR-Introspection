a46aaeff42a9eba7c304249a905080d3f1441723
cailllev <cailllev@students.zhaw.ch>
2025-08-19 11:59:00 +0200

    add instant startup, add time start

--- Injector/inject.cpp
+++ Injector/inject.cpp
@@ -1,27 +1,38 @@
 #include <windows.h>
 #include <iostream>
+#include <iomanip>
+#include <ctime>
+
+// print current timestamp in ISO 8601 format, e.g. 2025-08-18 18:03:51Z
+void printCurrentTime(std::string prefix) {
+    std::time_t now = std::time(nullptr);
+    std::tm tmUtc{};
+    gmtime_s(&tmUtc, &now);
+    std::cout << prefix << std::put_time(&tmUtc, "%Y-%m-%d %H:%M:%SZ") << "\n";
+}
 
 int main(int argc, char** argv) {
-    bool sleep = false;
+	enum StartupMode { NoWait, WaitTime, WaitForEnter };
+	StartupMode s = WaitForEnter; // default mode
     int waitTime = 0;
 
     if (argc < 2) {
-        sleep = false;
+        s = WaitForEnter;
+    }
+    else if (strcmp(argv[1], "--no-wait") == 0) {
+        s = NoWait;
     }
     else if (strcmp(argv[1], "--wait") == 0) {
-        sleep = true;
+        s = WaitTime;
         if (argc < 3) {
-			waitTime = 3; // default wait time
+            waitTime = 3; // default wait time
         }
         else {
             waitTime = strtol(argv[2], NULL, 10);
         }
     }
-    else {
-        sleep = false;
-	}
 
-    // start Notepad
+    // start calc
     STARTUPINFO si = { sizeof(si) };
     PROCESS_INFORMATION pi;
     if (!CreateProcess(L"C:\\Windows\\System32\\notepad.exe", nullptr, nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi)) {
@@ -30,15 +41,21 @@ int main(int argc, char** argv) {
     }
 
     std::cout << "Notepad started. PID: " << pi.dwProcessId << "\n";
-    if (sleep) {
-        for (int i = waitTime; i > 0; i--) {
-            std::cout << "Starting injection in " << i << "\n";
-            Sleep(1000);
-        }
-    }
-    else {
-		std::cout << "Press ENTER to start injection...\n";
-		std::cin.get();
+	printCurrentTime("Process started at: ");
+    switch (s) {
+        case NoWait:
+            break;
+        case WaitTime:
+            for (int i = waitTime; i > 0; i--) {
+                std::cout << "Starting injection in " << i << "\n";
+                Sleep(1000);
+            };
+            break;
+        case WaitForEnter:
+            std::cout << "Press ENTER to start injection...\n";
+            std::cin.get();;
+        default:
+            break;
     }
 
     // open process with read/write access
@@ -52,13 +69,14 @@ int main(int argc, char** argv) {
     int valueToWrite = 0xdeadbeaf;
     LPVOID remoteAddr = VirtualAllocEx(hProcess, nullptr, sizeof(valueToWrite), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (remoteAddr) {
-		std::cout << "Memory allocated at address: " << remoteAddr << "\n";
-    } else {
+        std::cout << "Memory allocated at address: " << remoteAddr << "\n";
+    }
+    else {
         std::cerr << "Failed to allocate memory: " << GetLastError() << "\n";
         return 1;
     }
 
-	// read before writing
+    // read before writing
     SIZE_T bytesRead;
     int valueRead = 0;
     if (ReadProcessMemory(hProcess, remoteAddr, &valueRead, sizeof(valueRead), &bytesRead)) {
@@ -79,6 +97,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    printCurrentTime("Value written at: ");
 
     // and read it
     if (ReadProcessMemory(hProcess, remoteAddr, &valueRead, sizeof(valueRead), &bytesRead)) {
@@ -96,7 +115,7 @@ int main(int argc, char** argv) {
     }
     else {
         std::cerr << "Failed to terminate Notepad: " << GetLastError() << "\n";
-	}
+    }
     CloseHandle(pi.hThread);
     CloseHandle(pi.hProcess);
 
