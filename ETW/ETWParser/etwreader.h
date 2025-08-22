b8305a8ff367dc22a16da2180958b2e4534a28d8
cailllev <cailllev@students.zhaw.ch>
2025-08-22 10:24:20 +0200

    compiles

--- ETW/ETWParser/etwreader.h
+++ ETW/ETWParser/etwreader.h
@@ -11,9 +11,9 @@ bool start_etw_reader(std::vector<HANDLE>& threads);
 void stop_etw_reader();
 std::vector<std::string> get_events();
 
-std::string TYPE = "type";
-std::string TIMESTAMP = "timestamp";
-std::string PID = "PID";
-std::string TASK = "task";
-std::string EVENT_ID = "event_id";
-std::string PROVIDER_NAME = "provider_name";
+static const std::string TYPE = "type";
+static const std::string TIMESTAMP = "timestamp";
+static const std::string PID = "PID";
+static const std::string TASK = "task";
+static const std::string EVENT_ID = "event_id";
+static const std::string PROVIDER_NAME = "provider_name";
