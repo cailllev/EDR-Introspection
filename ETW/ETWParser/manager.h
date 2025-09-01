4b4d9878175095f5d374a656c1e40907a1d0c9e9
cailllev <cailllev@students.zhaw.ch>
2025-09-01 11:56:05 +0200

    add en/decryption support for attack exe

--- ETW/ETWParser/manager.h
+++ ETW/ETWParser/manager.h
@@ -6,11 +6,11 @@
 
 
 // defaults
-std::string all_events_output_default = "C:\\Users\\Public\\Downloads\\all-events.csv";
+static const std::string all_events_output_default = "C:\\Users\\Public\\Downloads\\all-events.csv";
 
 
 // define start of CSV header, all other keys are added in order later
 std::vector<std::string> csv_header_start = { 
-    TIMESTAMP, TYPE, PROVIDER_NAME, EVENT_ID, TASK, PID, EXE, ppid_keys.merged_key, "TargetPID", "Message", 
+    TIMESTAMP, TYPE, PROVIDER_NAME, EVENT_ID, TASK, PID, TID, EXE, ppid_keys.merged_key, tpid_keys.merged_key, "Message",
     filepath_keys.merged_key, "Cache Name", "Result", "VName", "SigSeq", "SigSha", "Command Line", "First Param", "Second Param",
 };
