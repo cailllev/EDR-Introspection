d2b5b42fe1bc5fe68c2beb2b4c6ef3ce37bfae4d
cailllev <cailllev@students.zhaw.ch>
2025-09-04 14:45:10 +0200

    mutex

--- EDRi/utils.h
+++ EDRi/utils.h
@@ -3,16 +3,21 @@
 #include <map>
 #include <string>
 
-// all stolen from https://github.com/dobin/RedEdr
+#include "globals.h"
 
+static const std::string PROC_NOT_FOUND = "<not found>";
+
+// all stolen from https://github.com/dobin/RedEdr
 std::string wchar2string(const wchar_t* wstr);
 std::string wstring2string(std::wstring& wide_string);
 bool wstring_starts_with(const std::wstring& str, const std::wstring& prefix);
 std::string filetime_to_iso8601(__int64 timestamp);
-
 char* get_memory_region_protect(DWORD protect);
 
-int get_PID_by_name(std::map<int, std::string>, std::string);
-std::map<int, std::string> snapshot_procs();
+// own cacophony
+void snapshot_procs(bool);
+int get_PID_by_name(const std::string& name);
+void add_proc(int, const std::string&);
+std::string get_proc_name(int);
 
 bool xor_file(std::string, std::string);
