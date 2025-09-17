c852221f41a03d2746f602ab4822a673b0b3c573
cailllev <cailllev@students.zhaw.ch>
2025-09-17 11:57:35 +0200

    refactor ETW-TI

--- EDRi/etwreader.h
+++ EDRi/etwreader.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 
+static const std::wstring ETW_TI_PROVIDER_GUID_W = L"{b675ec37-bdb6-4648-bc92-f3fdc74d3ca2}";
 
 bool start_etw_traces(std::vector<HANDLE>& threads);
 bool start_etw_ti_traces(std::vector<HANDLE>& threads);
