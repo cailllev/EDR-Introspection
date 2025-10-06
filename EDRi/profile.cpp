4b204001a395a33501c72b12326251c31995fe1d
cailllev <cailllev@students.zhaw.ch>
2025-10-06 20:45:25 +0200

    cortex work

--- EDRi/profile.cpp
+++ EDRi/profile.cpp
@@ -7,7 +7,7 @@
 static const std::map<std::string, std::vector<std::string>> edr_profiles = {
 	{ "Defender", {"MsMpEng.exe", "MpDefenderCoreService.exe"} },
     { "MDE", {"MsMpEng.exe", "MpDefenderCoreService.exe", "MsSense.exe", "SenseCnCProxy.exe", "SenseIR.exe", "SenseCE.exe", "SenseSampleUploader.exe", "SenseNdr.exe", "SenseSC.exe", "SenseCM.exe", "SenseTVM.exe"} },
-    { "Cortex", {"TODO"} } // TODO
+    { "Cortex", {"cyserver.exe", "cysandbox.exe", "cywscsvc.exe", "tlaworker.exe", "cortex-xdr-payload.exe", "cyuserserver.exe"}} // TODO multiple procs, multiple main exes
 };
 
 std::string get_available_edrs() {
