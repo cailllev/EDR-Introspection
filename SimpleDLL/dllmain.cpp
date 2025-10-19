6e3e824ba59dec1d1fb3b66a69503a7746aa931c
cailllev <cailllev@students.zhaw.ch>
2025-10-19 21:35:27 +0200

    updates

--- SimpleDLL/dllmain.cpp
+++ SimpleDLL/dllmain.cpp
@@ -2,23 +2,21 @@
 #include <windows.h>
 #include <fstream>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
-{
-    switch (ul_reason_for_call)
+void write_file(std::string msg) {
+    std::ofstream outfile("C:\\Users\\Public\\Downloads\\SimpleDLL-out.txt");
+    if (outfile.is_open())
+    {
+        outfile << msg;
+        outfile.close();
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  reasonForCall, LPVOID lpReserved) {
+    switch (reasonForCall)
     {
     case DLL_PROCESS_ATTACH:
-    {
-        std::ofstream outfile("C:\\Users\\Public\\Downloads\\SimpleDLL-out.txt");
-        if (outfile.is_open())
-        {
-            outfile << "ok";
-            outfile.close();
-        }
+        write_file("dll loaded");
         break;
-    }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
