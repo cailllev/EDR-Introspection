c73456c173f39995d5efc0e8a07318c9423a3f3b
cailllev <cailllev@students.zhaw.ch>
2025-11-03 18:39:14 +0100

    building works

--- attacks/Injector/build-features.bat
+++ attacks/Injector/build-features.bat
@@ -1,3 +1,5 @@
-for %%C in (standard encryption anti_emultaion_calc anti_emultaion_sleep deconditioning_calc deconditioning_alloc) do (
-  "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" Injector.vcxproj /p:Configuration=%%C /p:Platform=x64
+for %%C in (standard obfuscation anti_emulation_calc anti_emulation_sleep deconditioning_calc deconditioning_alloc) do (
+  "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" Injector.vcxproj /p:Configuration=%%C /p:Platform=x64 /p:SolutionDir=..\..\
+  echo "Encrypting the created Injector-%%C.exe"
+  ..\..\x64\Release\EDRi.exe -e ..\..\x64\Release\attacks\Injector-%%C.exe
 )
