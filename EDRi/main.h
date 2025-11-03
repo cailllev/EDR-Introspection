ddc7210746af5497f502d25f41436a7e2958c945
cailllev <cailllev@students.zhaw.ch>
2025-11-03 18:41:34 +0100

    remove unused

--- EDRi/main.h
+++ EDRi/main.h
@@ -1,12 +1,1 @@
 #pragma once
-
-#include "etwparser.h"
-#include "profile.h"
-
-#include <vector>
-#include <string>
-
-
-// defaults
-static const std::string all_events_output_default = "C:\\Users\\Public\\Downloads\\all-events.csv";
-
