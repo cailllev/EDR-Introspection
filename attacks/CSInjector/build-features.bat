442a0c875e377234dd3d3471717e3c5507f5beea
cailllev <cailllev@students.zhaw.ch>
2025-11-21 15:51:52 +0100

    rename

--- attacks/CSInjector/build-features.bat
+++
@@ -1,5 +0,0 @@
-for %%C in (standard obfuscation anti_emulation_calc anti_emulation_sleep deconditioning_calc deconditioning_alloc) do (
-  "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" CSInjector.vcxproj /p:Configuration=%%C /p:Platform=x64 /p:SolutionDir=..\..\
-  echo "Encrypting the created CSInjector-%%C.exe"
-  ..\..\x64\Release\EDRi.exe -e ..\..\x64\Release\attacks\CSInjector-%%C.exe
-)
