76c6b9feedc1cb6a026d080cedbf7e3feb69e67e
cailllev <cailllev@students.zhaw.ch>
2025-11-21 11:27:11 +0100

    new attack (tm)

--- attacks/CSInjector/build-features.bat
+++ attacks/CSInjector/build-features.bat
@@ -1,5 +1,5 @@
 for %%C in (standard obfuscation anti_emulation_calc anti_emulation_sleep deconditioning_calc deconditioning_alloc) do (
-  "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" Injector.vcxproj /p:Configuration=%%C /p:Platform=x64 /p:SolutionDir=..\..\
-  echo "Encrypting the created Injector-%%C.exe"
-  ..\..\x64\Release\EDRi.exe -e ..\..\x64\Release\attacks\Injector-%%C.exe
+  "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" CSInjector.vcxproj /p:Configuration=%%C /p:Platform=x64 /p:SolutionDir=..\..\
+  echo "Encrypting the created CSInjector-%%C.exe"
+  ..\..\x64\Release\EDRi.exe -e ..\..\x64\Release\attacks\CSInjector-%%C.exe
 )
