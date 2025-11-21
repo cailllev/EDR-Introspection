5c8f02b279f8b0c64b775af5660ea4e0b6c95900
cailllev <cailllev@students.zhaw.ch>
2025-11-21 15:52:04 +0100

    rename

---
+++ attacks/CalcExample/build-features.bat
@@ -0,0 +1,5 @@
+for %%C in (standard obfuscation anti_emulation_calc anti_emulation_sleep deconditioning_calc deconditioning_alloc) do (
+  "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" CalcExample.vcxproj /p:Configuration=%%C /p:Platform=x64 /p:SolutionDir=..\..\
+  echo "Encrypting the created CalcExample-%%C.exe"
+  ..\..\x64\Release\EDRi.exe -e ..\..\x64\Release\attacks\CalcExample-%%C.exe
+)
