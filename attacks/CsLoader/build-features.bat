69be89827e25d5c82c6612436cc55bf72cbd5ae7
cailllev <cailllev@students.zhaw.ch>
2025-11-29 12:55:29 +0100

    update bypasses

--- attacks/CsLoader/build-features.bat
+++ attacks/CsLoader/build-features.bat
@@ -1,4 +1,4 @@
-for %%C in (standard obfuscation anti_emulation_calc anti_emulation_sleep deconditioning_calc deconditioning_alloc) do (
+for %%C in (standard obfuscation antiEmulation deconditioning) do (
   "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" CsLoader.vcxproj /p:Configuration=%%C /p:Platform=x64 /p:SolutionDir=..\..\
   echo "Encrypting the created CsLoader-%%C.exe"
   ..\..\x64\Release\EDRi.exe -e ..\..\x64\Release\attacks\CsLoader-%%C.exe
