ccf6c72ffc7c456ebd4d843dd54a138db3fc0cac
evilele <evilele@pm.me>
2026-08-18 00:33:08 +0200

    rework parameters

--- InjectLoader/loader.cpp
+++ InjectLoader/loader.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
     std::string exePath = argv[0];
     std::string exeName = exePath.substr(exePath.find_last_of("\\/") + 1);
     std::string usage = "";
-    usage += "[*] InjectLoader: Usage: " + exeName + " <DLL Path> <Target PID> <(L)oadLibrary | (E)xternal | (R)eflective | (S)top> <Debug> <FindProcHandle> <(C)reateRemoteThread | (H)ijackThread | (Q)ueueUserAPC>\n";
+    usage += "[*] InjectLoader: Usage: " + exeName + " <Target PID> <FindProcHandle> <DLL Path> <(L)oadLibrary | (H)ostMapped | (R)eflective | (S)top> <(C)reateRemoteThread | (H)ijackThread | (Q)ueueUserAPC> [RiskInstability] [Debug]\n";
     usage += "[*] InjectLoader: Usage: " + exeName + " C:\\path\\to\\dll.dll 1234 LoadLibrary 1 1 1\n";
     usage += "[*] InjectLoader: Limitations against EDRs: \n";
     usage += "      - all inject techniques VM_Operation and VM_Write (usually denied via KernelCallbacks)\n";
@@ -27,79 +27,97 @@ int main(int argc, char* argv[]) {
         std::cout << usage;
         return 0;
     }
-
-    if (argc < 7) {
+    if (argc < 8) {
         std::cout << usage;
         return 1;
     }
 
-    dllPath = argv[1];
     try {
-        pid = std::stoi(argv[2]);
+        pid = std::stoi(argv[1]);
+        if (pid <= 0) {
+            std::cerr << "[!] InjectLoader: PID must be a positive integer.\n";
+            return 1;
+        }
     }
     catch (const std::exception&) {
         std::cerr << "[!] InjectLoader: Invalid PID: " << argv[2] << "\n";
         return 1;
     }
 
-    if (_stricmp(argv[3], "S") == 0 || _stricmp(argv[3], "stop") == 0) {
+    bool findProcHandle = false;
+    if (argv[2][0] == '1') {
+        findProcHandle = true;
+    }
+
+    dllPath = argv[3];
+    if (dllPath.empty()) {
+        std::cerr << "[!] InjectLoader: DLL path cannot be empty.\n";
+        return 1;
+    }
+
+    if (_stricmp(argv[4], "S") == 0 || _stricmp(argv[4], "stop") == 0) {
         std::cout << "[*] InjectLoader: Unloading DLL in " << pid << "\n";
         std::string dllName = dllPath.substr(dllPath.find_last_of("\\/") + 1);
         return UnloadViaThread(pid, dllName);
     }
 
     Injection injectType;
-    if (_stricmp(argv[3], "R") == 0 || _stricmp(argv[3], "reflective") == 0) {
-        injectType = REFLECTIVE_INJECTION;
-    }
-    else if (_stricmp(argv[3], "E") == 0 || _stricmp(argv[3], "external") == 0) {
-        injectType = HOSTMAPPED_INJECTION;
-    }
-    else {
+    if (_stricmp(argv[4], "L") == 0 || _stricmp(argv[4], "loadlibrary") == 0) {
         injectType = LOADLIBRARY_INJECTION;
     }
-	std::string actionStr = GetInjectTypeStr(injectType);
-
-    if (pid <= 0) {
-        std::cerr << "[!] InjectLoader: PID must be a positive integer.\n";
+    else if (_stricmp(argv[4], "H") == 0 || _stricmp(argv[4], "hostmapped") == 0) {
+        injectType = HOSTMAPPED_INJECTION;
+    }
+    else if (_stricmp(argv[4], "R") == 0 || _stricmp(argv[4], "reflective") == 0) {
+        injectType = REFLECTIVE_INJECTION;
+    }
+    else {
+        std::cerr << "[!] InjectLoader: InjectType '" << argv[4] << " 'not defined.\n";
         return 1;
     }
-    if (dllPath.empty()) {
-        std::cerr << "[!] InjectLoader: DLL path cannot be empty.\n";
+
+    Executor execType;
+    if (_stricmp(argv[5], "C") == 0 || _stricmp(argv[5], "createremotethread") == 0) {
+        execType = CREATE_REMOTE_THREAD;
+    }
+    else if (_stricmp(argv[5], "H") == 0 || _stricmp(argv[5], "hijackthread") == 0) {
+		execType = HIJACK_THREAD;
+	}
+	else if (_stricmp(argv[5], "Q") == 0 || _stricmp(argv[5], "queueuserapc") == 0) {
+		execType = QUEUE_USER_APC2;
+	}
+    else {
+        std::cerr << "[!] InjectLoader: ExecType '" << argv[5] << " 'not defined.\n";
         return 1;
+    }
+
+    bool yoloMode = false;
+    if (argc > 6 && argv[6][0] == '1') {
+        std::cout << "[$] InjectLoader: Yolo mode activated, using any thread and ignore missing imports\n";
+        yoloMode = true;
     }
 
     bool debug = false;
-    if (argv[4][0] == '1') {
+    if (argc > 7 && argv[7][0] == '1') {
         std::cout << "[*] InjectLoader: Debug mode enabled.\n";
         debug = true;
     }
 
     HANDLE hProc = NULL;
-    if (argv[5][0] == '1') {
-        std::cout << "[*] InjectLoader: Process handle already opened, searching for it...\n";
+    if (findProcHandle) {
+        std::cout << "[*] InjectLoader: Process handle should already exist in this injector process -> searching for it...\n";
         hProc = FindProcHandle(pid, debug);
-		if (hProc == NULL) {
-			std::cerr << "[!] InjectLoader: Failed to find process handle for PID " << pid << ".\n";
-			return 1;
-		}
-		else {
-			std::cout << "[*] InjectLoader: Found process handle: " << hProc << "\n";
-		}
+        if (hProc == NULL) {
+            std::cerr << "[!] InjectLoader: Failed to find process handle for PID " << pid << ".\n";
+            return 1;
+        }
+        else {
+            std::cout << "[*] InjectLoader: Found process handle: " << hProc << "\n";
+        }
     }
 
-    Executor execType;
-    if (_stricmp(argv[6], "H") == 0 || _stricmp(argv[6], "hijackthread") == 0) {
-		execType = HIJACK_THREAD;
-	}
-	else if (_stricmp(argv[6], "Q") == 0 || _stricmp(argv[6], "queueuserapc") == 0) {
-		execType = QUEUE_USER_APC2;
-	}
-	else {
-		execType = CREATE_REMOTE_THREAD;
-	}
+    std::string actionStr = GetInjectTypeStr(injectType);
     std::string execStr = GetExecutorTypeStr(execType);
-
-    std::cout << "[*] InjectLoader: Attempting to inject DLL '" << dllPath << "' into PID=" << pid << " using " << actionStr << " injection method and " << execStr << " execution.\n";
-    return InjectDll(pid, dllPath, hProc, injectType, execType, debug);
+    std::cout << "[*] InjectLoader: Attempting to inject DLL '" << dllPath << "' into PID=" << pid << " using " << actionStr << " injection method and " << execStr << " execution\n";
+    return InjectDll(pid, dllPath, hProc, injectType, execType, yoloMode, debug);
 }
