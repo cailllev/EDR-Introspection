5a30554f3c3dd77c5f5d4d116356f449aebcb0cb
Levi Cailleret <70950391+evilele@users.noreply.github.com>
2026-08-20 13:20:30 +0200

    create filecopy dll

---
+++ misc/FileCopier/dllmain.cpp
@@ -0,0 +1,19 @@
+// dllmain.cpp
+#include <windows.h>
+#include <string>
+#include <stdio.h>
+
+std::wstring src = L"C:\\ProgramData\\Microsoft\\Windows Defender\\Scans\\mpenginedb.db";
+std::wstring dst = L"C:\\file.txt"
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+        if (CopyFileW(src.c_str(), dst.c_str(), FALSE)) { // overwrite destination if it exists
+            wprintf(L"[*] Copied %s to %s\n", src.c_str(), dst.c_str());
+        } else {
+            wprintf(L"[*] Failed to copy %s to %s. Error: %i\n", src.c_str(), dst.c_str(), GetLastError());
+        }
+    }
+
+    return TRUE;
+}
