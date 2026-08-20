fec729e05f8087ec7935f77fc5454799f332e1b5
Levi Cailleret <70950391+evilele@users.noreply.github.com>
2026-08-20 15:06:36 +0200

    fix dll

--- misc/FileCopier/dllmain.cpp
+++ misc/FileCopier/dllmain.cpp
@@ -4,14 +4,16 @@
 #include <stdio.h>
 
 std::wstring src = L"C:\\ProgramData\\Microsoft\\Windows Defender\\Scans\\mpenginedb.db";
-std::wstring dst = L"C:\\file.txt"
+std::wstring dst = L"C:\\file.txt";
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
         if (CopyFileW(src.c_str(), dst.c_str(), FALSE)) { // overwrite destination if it exists
             wprintf(L"[*] Copied %s to %s\n", src.c_str(), dst.c_str());
+            return TRUE;
         } else {
             wprintf(L"[*] Failed to copy %s to %s. Error: %i\n", src.c_str(), dst.c_str(), GetLastError());
+            return FALSE;
         }
     }
 
